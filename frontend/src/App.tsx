2f3500a4dfb257171252a950187a33a3e9e124da
Anderson Nieves <andersonvnieves@live.com>
2025-09-01 18:05:42 -0300

    Added broto-ui lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "broto-ui-react"
+
 function App() {
 
   return (
     <section className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="font-bold text-center text-4xl">mangos</h1>
       <h2 className="text-center text-2xl">under construction !</h2>
+      <Button primary={true} label="Click me" onClick={() => alert("Button clicked!")}></Button>
     </section>
   )
 }
